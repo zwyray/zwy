959e127025c536308f16975676d81698055f7f26
zwyray <zwyray@deepin.com>
2018-07-17 01:34:18 +0800

    优化

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,13 @@ module Zwy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.i18n.default_locale = "zh-CN"
+    config.time_zone = 'Beijing'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
+Date::DATE_FORMATS[:short] = "%Y/%m/%d"
+Time::DATE_FORMATS[:short] = "%Y/%m/%d %H:%M"
