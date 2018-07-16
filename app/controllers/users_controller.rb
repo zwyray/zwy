2671ec3789ebf643d552ee8906609ccae935eb03
zwyray <zwyray@deepin.com>
2018-07-17 00:51:50 +0800

    delete bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,20 +63,11 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :url, :ip, :port, :host, :agent)
     end
 end
